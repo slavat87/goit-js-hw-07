584bb671a0ee69f067e826f778684809ab95a154
Slavat <tymosh@gmail.com>
2020-12-05 20:25:35 +0100

    Update task-1.js

--- JS/task-1.js
+++ JS/task-1.js
@@ -3,6 +3,9 @@
 // Вийде 'У списку 3 категорії.'.
 // Для кожного елемента li.item в списку ul#categories, знайде і виведе в консоль текст заголовка елемента (тега h2) і кількість елементів в категорії (всіх вкладених в нього елементів li).
 
+const listLengthRef = document.querySelectorAll("h2").length;
+console.log(`У списку ${listLengthRef} категорії.`);
+
 const elements = document.querySelectorAll(".item");
 elements.forEach(function (element) {
   console.log(`Категорія: ${element.firstElementChild.textContent}`);
